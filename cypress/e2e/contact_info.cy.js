145db5a254a2b5bea87ad6c6d43d12c4bee8e905
Tatiana Shuliak <tatjananikolaevich23041997@gmail.com>
2023-08-26 13:49:02 +0300

    test added

---
+++ cypress/e2e/contact_info.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe("checking contact info", () => {
+    it("checks is email correct", () => {
+    cy.visit("https://google.com");
+    cy.get("[name='q']").type("Byndyusoft");
+    cy.get('.FPdoLc > center > .gNO89b').click();
+    cy.get('h3', {timeout: 10000}).first().click();
+    cy.origin('https://byndyusoft.com', () => {
+    cy.get('.btn--info', {timeout: 10000}).click();
+    cy.contains("sales@byndyusoft.com ").should("be.visible");
+    });
+    });
+});
