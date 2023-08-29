ded4cf8f6b9748af5e968e6055897a36181932f4
Tatiana Shuliak <tatjananikolaevich23041997@gmail.com>
2023-08-29 19:55:59 +0300

    test telegram link added

--- cypress/e2e/contact_info.cy.js
+++ cypress/e2e/contact_info.cy.js
@@ -8,7 +8,17 @@ describe("checking contact info", () => {
     cy.get('h3', {timeout: 10000}).first().click();
     cy.origin('https://byndyusoft.com', () => {
     cy.get('.btn--info', {timeout: 10000}).click();
-    cy.contains("sales@byndyusoft.com ").should("be.visible");
+    cy.contains("sales@byndyusoft.com").should("be.visible");
     });
     });
+    it("checks is telegram link correct", () => {
+        cy.visit("https://google.com");
+        cy.get("[name='q']").type("Byndyusoft");
+        cy.get('.FPdoLc > center > .gNO89b').click();
+        cy.get('h3', {timeout: 10000}).first().click();
+        cy.origin('https://byndyusoft.com', () => {
+        cy.get('.btn--info', {timeout: 10000}).click();
+        cy.get("a[href='http://t.me/alexanderbyndyu']").should("exist");
+        });
+        });
 });
